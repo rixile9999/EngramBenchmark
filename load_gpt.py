3c8fedf72e5ebba07f153ed4d4df53f8b5a606ba
Hyoseok <rixile9999@gmail.com>
2025-04-26 18:19:32 +0900

    added .gitignore

--- load_gpt.py
+++ load_gpt.py
@@ -31,10 +31,8 @@ def run_openai_chat(prompt: str, messages):
     response = openai_chat.invoke(messages)
     return response.content
 
-def run_chatgpt_with_examples(query, examples, input, num_gen=1, num_tokens_request=1000, use_16k=False, wait_time = 1, temperature=1.0):
 
-    completion = None
-    
+def run_chatgpt_with_examples(query, examples, input):
     messages = [
         {"role": "system", "content": query}
     ]
@@ -48,36 +46,5 @@ def run_chatgpt_with_examples(query, examples, input, num_gen=1, num_tokens_requ
     messages.append(
         {"role": "user", "content": input}
     )   
-
-    while completion is None:
-        wait_time = wait_time * 2
-        try:
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo" if not use_16k else "gpt-3.5-turbo-16k",
-                temperature = temperature,
-                max_tokens = num_tokens_request,
-                n=num_gen,
-                messages = messages
-            )
-            
-        except openai.error.APIError as e:
-            #Handle API error here, e.g. retry or log
-            print(f"OpenAI API returned an API Error: {e}; waiting for {wait_time} seconds")
-            time.sleep(wait_time)
-            pass
-        except openai.error.APIConnectionError as e:
-            #Handle connection error here
-            print(f"Failed to connect to OpenAI API: {e}; waiting for {wait_time} seconds")
-            time.sleep(wait_time)
-            pass
-        except openai.error.RateLimitError as e:
-            #Handle rate limit error (we recommend using exponential backoff)
-            print(f"OpenAI API request exceeded rate limit: {e}")
-            pass
-        except openai.error.ServiceUnavailableError as e:
-            #Handle rate limit error (we recommend using exponential backoff)
-            print(f"OpenAI API request exceeded rate limit: {e}; waiting for {wait_time} seconds")
-            time.sleep(wait_time)
-            pass
-    
-    return completion.choices[0].message.content
+    result = openai_chat.invoke(messages)
+    return result.content
